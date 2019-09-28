3b59bfe570ea7e85f5e32aa13e21ec634347ee45
= <m.frasson.jr@gmail.com>
2019-09-28 13:30:17 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/mfrasson/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,10 @@
+package com.mfrasson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mfrasson.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}

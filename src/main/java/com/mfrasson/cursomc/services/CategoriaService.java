523476063c2e82bbf33ec7a32fbbdccc0b97f950
= <m.frasson.jr@gmail.com>
2019-09-28 19:28:44 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/mfrasson/cursomc/services/CategoriaService.java
+++ src/main/java/com/mfrasson/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.mfrasson.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mfrasson.cursomc.domain.Categoria;
 import com.mfrasson.cursomc.repositories.CategoriaRepository;
+import com.mfrasson.cursomc.services.exceptions.DataIntegrityException;
 import com.mfrasson.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }

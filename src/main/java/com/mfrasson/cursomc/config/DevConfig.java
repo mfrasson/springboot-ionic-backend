d5504c716456956db2b4e16bf3950ca3ef96a66a
= <m.frasson.jr@gmail.com>
2019-10-03 20:21:57 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/mfrasson/cursomc/config/DevConfig.java
+++ src/main/java/com/mfrasson/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.mfrasson.cursomc.services.DBService;
+import com.mfrasson.cursomc.services.EmailService;
+import com.mfrasson.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,4 +31,8 @@ public class DevConfig {
 		return true;
 	}
 	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
 }
